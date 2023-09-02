dc49bbcacf297eaae4c85545c4582dc55cefaba5
minsu <zmfjtnl1@naver.com>
2023-09-02 15:50:33 +0900

    CounterTest payTest add

--- src/test/java/com/nhnacademy/mart/CounterTest.java
+++ src/test/java/com/nhnacademy/mart/CounterTest.java
@@ -1,4 +1,19 @@
+package com.nhnacademy.mart;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class CounterTest {
-  
+
+    @Test
+    @DisplayName("CounterTest pay() Method Test")
+    void payTest() {
+        Basket basket = new Basket();
+        Counter counter = new Counter();
+        basket.add(new Food("양파", 1000));
+        Assertions.assertEquals(counter.pay(basket), 1000);
+    }
 }
