95d9c463186d65246df8b10a747cb9a013fc2c37
minsu <zmfjtnl1@naver.com>
2023-09-02 15:44:12 +0900

    BasketTest code modified

--- src/test/java/com/nhnacademy/mart/BasketTest.java
+++ src/test/java/com/nhnacademy/mart/BasketTest.java
@@ -2,6 +2,8 @@ package com.nhnacademy.mart;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,11 @@ class BasketTest {
     @Test
     @DisplayName("Basket add() Method Test")
     void add() {
+        Basket basket = new Basket();
+        ArrayList<Food> foods = new ArrayList<>();
+        foods.add(new Food("양파", 1000));
+        basket.add(new Food("양파", 1000));
+        Assertions.assertEquals(basket.getFoods().get(0).toString(),foods.get(0).toString());
 
     }
 }
