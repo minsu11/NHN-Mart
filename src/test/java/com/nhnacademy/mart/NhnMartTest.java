190df4b98c3e5e8bc7da675836dff9957e821f6e
minsu <zmfjtnl1@naver.com>
2023-09-02 22:51:07 +0900

    NhnMartTest Modified

--- src/test/java/com/nhnacademy/mart/NhnMartTest.java
+++ src/test/java/com/nhnacademy/mart/NhnMartTest.java
@@ -2,12 +2,14 @@ package com.nhnacademy.mart;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class NhnMartTest {
-
+    // NhnMartTest 파일은 추가 하였으나, 따로 테스트가 필요하지 않은 class라고 생각이 들어서 작성하지 않았습니다.
     @Test
     void prepareMart() {
+
     }
 
     @Test
