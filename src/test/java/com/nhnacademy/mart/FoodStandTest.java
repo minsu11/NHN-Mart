c53a2c4ad65495d4a542eb0e5169df77ebaec955
minsu <zmfjtnl1@naver.com>
2023-09-02 19:26:58 +0900

    FoodStanTest add() Method Test Add

--- src/test/java/com/nhnacademy/mart/FoodStandTest.java
+++ src/test/java/com/nhnacademy/mart/FoodStandTest.java
@@ -2,6 +2,8 @@ package com.nhnacademy.mart;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +12,13 @@ class FoodStandTest {
     @Test
     @DisplayName("foodStand add() Method Test")
     void addFoodStandTest() {
-
+        FoodStand foodStand = new FoodStand();
+        ArrayList<Food> foods = new ArrayList<>();
+        foodStand.add(new Food("양파",1000));
+        foods.add(new Food("양파", 1000));
+        Assertions.assertEquals(foodStand.getFoods().toString(),foods.toString());
     }
+
 
     @Test
     void delete() {
